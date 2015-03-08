ee4c2362f9697d6a38ce943b82c4eb06271c1bb4
Jungle <wwjd228@gmail.com>
2015-03-09 00:37:19 +0800

    add some code for test the time

--- lab-1/fibseq.c
+++ lab-1/fibseq.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <time.h>
 
 extern int fibonacci(int x);
 
 int main(int argc, char **argv)
 {
+  clock_t start_time, end_time;
+  float total_time;
   int number=0;
   int result=0;
 
   scanf("%d",&number);
-  result = fibonacci(number);   
+  start_time = clock();
+  result = fibonacci(number);
+  end_time = clock();
+  total_time = (float) end_time - start_time;
   printf("The fibonacci sequence at %d is: %d\n", number, result);
+  printf( "Time = %f ms\n", total_time );
 }
 
